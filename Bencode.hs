79fdb3765d4506ff8ba3c0ba2cef247ae34d1365
Christopher Sasarak <chris.sasarak@gmail.com>
2013-05-21 15:24:55 -0400

    Added a parser for bencoded lists

--- Bencode.hs
+++ Bencode.hs
@@ -5,17 +5,31 @@ module Bencode where
 
 import Text.ParserCombinators.Parsec
 
+data Bencode =  Bint Integer
+              | Bstr String
+              | Blist [Bencode]
+              deriving (Show)
+
+
+
+
 -- Parse a Bencoded Integer
-integer :: Parser Integer
-integer = do char 'i'
-             ds <- many1 digit
-             char 'e'
-             return $ read ds 
+bInt :: Parser Bencode
+bInt = do char 'i'
+          ds <- many1 digit
+          char 'e'
+          return $ Bint $ read ds 
 
 -- Parse a Bencoded String
-bString :: Parser String
+bString :: Parser Bencode
 bString = do ss <- many1 digit
              char ':'
              let size = read ss
-             count size $ anyChar
+             (count size $ anyChar) >>= \x -> return (Bstr x)
              
+bList :: Parser Bencode
+bList = do char 'l' 
+           ls <- many (bInt <|> bString <|> bList)
+           char 'e'
+           return $ Blist ls
+
