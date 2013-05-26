4108061c4c1d2bb544c9a66710a4d96f2413ece6
Christopher Sasarak <chris.sasarak@gmail.com>
2013-05-25 20:17:17 -0400

    naming update

--- test/BencodeTest.hs
+++ test/BencodeTest.hs
@@ -13,10 +13,10 @@ import System.IO
 -- its Bencoded form
 main :: IO ()
 main = do filename <- getArgs >>= (return . head)
-          bDict <- readTorrentFile filename
+          bDict <- readBencodedFile filename
           putStr $ show bDict
 
 -- This function reads a torrent file. readTorrentFile "filename" reads
 -- that filename and returns the parsed bencoded dictionary
-readTorrentFile :: String -> IO (Either PE.ParseError B.Bencode)
-readTorrentFile filename = BS.parseFromFile B.bDict filename
+readBencodedFile :: String -> IO (Either PE.ParseError B.Bencode)
+readBencodedFile filename = BS.parseFromFile B.bDict filename
