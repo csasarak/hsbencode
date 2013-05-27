b1126efd86799b7212bf7047e67db70a00329dbf
Christopher Sasarak <chris.sasarak@gmail.com>
2013-05-27 01:31:23 -0400

    Implemented the infoHash function

--- Tracker.hs
+++ Tracker.hs
@@ -4,3 +4,14 @@
 
 -- This module contains functions related to communicating with a bittorrent
 -- tracker
+
+module Tracker where
+
+import qualified Bencode as B
+import qualified Crypto.Hash.SHA1 as Crypt
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.ByteString as B
+
+-- Get a hash of this Dictionary
+infoHash :: B.Bencode -> B.ByteString
+infoHash = Crypt.hashlazy . BS.pack . show
